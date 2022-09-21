e96b0efd63ca3a641dc1aa977d237a828250722e
CouchMeka <meka.daniels1@gmail.com>
2022-09-21 15:54:34 -0400

    fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,14 +1,10 @@
 const prompt = require('prompt-sync')({sigint: true});
 
 let num = Number(prompt("Enter a number: "))
-function fibonacci(num) {
-    // x is representing the first term,
-    // y is representing the second term, and
-    // z is representing the sum of x and y.
-    let x = 0;
-    let y = 1;
-    let z;
-    let i = 2;
+let x = 0;
+let y = 1;
+let z;
+let i = 2;
     while (i < num) {
       z = x + y;
       x = y;
@@ -16,11 +12,10 @@ function fibonacci(num) {
   
       i = i + 1;
     }
-    return y;
-  }
+  
   
 
-  answer = fibonacci(num);
   
-  console.log(`The ${num}th number of the Fibonacci series is: ${answer}`);
+  
+  console.log(`The ${num}th number of the Fibonacci series is: ${y}`);
   
